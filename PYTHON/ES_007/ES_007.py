ae8c3ea3c740c3c9217aa66af5599cb27bae7899
Francesco Bruno <46680491+FraBrunoSchool@users.noreply.github.com>
2020-02-15 17:43:38 +0100

    ipList+ping

---
+++ PYTHON/ES_007/ES_007.py
@@ -0,0 +1,41 @@
+import subprocess
+
+
+def convert_bin(address):
+    ipaddress_bin = 0
+    for e, group in enumerate(address):
+        ipaddress_bin = ipaddress_bin + group * (2 ** (((3 - e) * 8)))
+        print(ipaddress_bin)
+
+    return ipaddress_bin
+
+
+def ping(address):
+    # Scegliere il Sistema operativo commentando la riga del sistema operativo non in uso
+    # windows
+    # p = subprocess.Popen(['ping', address], stdout=subprocess.PIPE)
+    # Linux
+    p = subprocess.Popen(['ping', '-c 2', address], stdout=subprocess.PIPE)
+    pingText = p.communicate()
+    print(pingText[0].decode())
+
+
+def address_list(address, mask):
+    for c in range(1, 2 ** (32 - mask) - 1):
+        address = address + 1
+        l = '.'.join([str(int(bin(address)[i:i + 8], 2)) for i in range(2, 34, 8)])
+        print(l)
+        ping(l)
+
+
+def main():
+    ipaddress = input("Inserisci un indirizzo IP: ")
+    mask = int(input("Inserisci una maschera: "))
+
+    ipaddress_splitted = [int(i) for i in ipaddress.split(".")]
+    ipaddress_host = convert_bin(ipaddress_splitted)
+    address_list(ipaddress_host, mask)
+
+
+if __name__ == '__main__':
+    main()
