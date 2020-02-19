440b01f877a5a54e98ee17bde29631bff5be7a18
Francesco Bruno <francesco.bruno@itiscuneo.eu>
2020-02-19 11:39:32 +0100

    grafi
    matrice di adiacenza grafo non orientato non pesato

---
+++ PYTHON/ES_GRAFONONORIENTATO/ES_GRAFONONORIENTATO.py
@@ -0,0 +1,19 @@
+def main():
+    V = int(input("Inserire il numero di nodi"))
+    grafo = []
+    for i in range(1, V + 1):
+        E = [int(i) for i in input(f"Inserire le vicinanze del nodo {i} (usare la ',' come separatore): ").split(',')]
+        colonna = []
+        for dim in range(0, V):
+            colonna.append(0)
+        for j in E:
+            if j != i:
+                print(j)
+            colonna[j-1] = 1
+        grafo.append(colonna)
+        print(E)
+    print(grafo)
+
+
+if __name__ == '__main__':
+    main()
