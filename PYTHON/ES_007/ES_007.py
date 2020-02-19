16ebd22b44017ccbf1d6c766cdd7c505d77faaad
Francesco Bruno <francesco.bruno@itiscuneo.eu>
2020-02-19 09:06:16 +0100

    ...
    ....

--- PYTHON/ES_007/ES_007.py
+++ PYTHON/ES_007/ES_007.py
@@ -5,7 +5,6 @@ def convert_bin(address):
     ipaddress_bin = 0
     for e, group in enumerate(address):
         ipaddress_bin = ipaddress_bin + group * (2 ** (((3 - e) * 8)))
-        print(ipaddress_bin)
 
     return ipaddress_bin
 
@@ -17,14 +16,19 @@ def ping(address):
     # Linux
     p = subprocess.Popen(['ping', '-c 2', address], stdout=subprocess.PIPE)
     pingText = p.communicate()
-    print(pingText[0].decode())
+    #print(pingText[0].decode())
+    if pingText[0].decode().count('100% packet loss') == 1:
+        print(f"L'host {address} non è attivo")
+    else:
+        if pingText[0].decode().count('100% packet loss') == 0:
+            print(f"L'host {address} è attivo")
 
 
 def address_list(address, mask):
     for c in range(1, 2 ** (32 - mask) - 1):
         address = address + 1
         l = '.'.join([str(int(bin(address)[i:i + 8], 2)) for i in range(2, 34, 8)])
-        print(l)
+        #print(l)
         ping(l)
 
 
