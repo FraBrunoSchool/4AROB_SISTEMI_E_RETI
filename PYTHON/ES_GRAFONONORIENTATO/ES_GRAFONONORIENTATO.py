187c247edc5c3ab16b89f7d1dd9bf4a2e0c93921
Francesco Bruno <francesco.bruno@itiscuneo.eu>
2020-02-20 09:56:13 +0100

    print+dict

--- PYTHON/ES_GRAFONONORIENTATO/ES_GRAFONONORIENTATO.py
+++ PYTHON/ES_GRAFONONORIENTATO/ES_GRAFONONORIENTATO.py
@@ -1,17 +1,47 @@
 def main():
-    V = int(input("Inserire il numero di nodi"))
+    v = int(input("Inserire il numero di nodi"))
+    grafo = creaMatrice(v)
+    stampaMatrice(grafo, v)
+    dict = adj2dict(grafo)
+    print(dict)
+
+
+def creaMatrice(v):
     grafo = []
-    for i in range(1, V + 1):
-        E = [int(i) for i in input(f"Inserire le vicinanze del nodo {i} (usare la ',' come separatore): ").split(',')]
-        colonna = []
-        for dim in range(0, V):
-            colonna.append(0)
-        for j in E:
+    for i in range(1, v + 1):
+        e = [int(i) for i in input(f"Inserire le vicinanze del nodo {i} (usare la ',' come separatore): ").split(',')]
+        colonna = [0 for dim in range(0, v)]
+        for j in e:
             if j != i:
                 colonna[j - 1] = 1
         grafo.append(colonna)
-    print(grafo)
+
+    return grafo
+
+
+def adj2dict(grafo):
+    dict = {}
+    for r in range(0, len(grafo)):
+        chiave = r + 1
+        occ = []
+        for c in range(0, len(grafo)):
+            if grafo[r][c] == 1:
+                occ.append(c+1)
+        dict[chiave] = occ
+
+    return dict
+
+
+def stampaMatrice(grafo, v):
+    for r in range(0, v):
+        print(" ")
+        for c in range(0, v):
+            print(grafo[r][c], end=' ')
 
 
 if __name__ == '__main__':
     main()
+
+"""
+dizionario delle adiacenze
+"""
