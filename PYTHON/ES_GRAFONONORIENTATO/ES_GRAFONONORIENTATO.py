300b654650eaab04f0bc69d7303c08ab45efc51f
Francesco Bruno <francesco.bruno2002@outlook.com>
2020-02-25 16:49:55 +0100

    grafo

--- PYTHON/ES_GRAFONONORIENTATO/ES_GRAFONONORIENTATO.py
+++ PYTHON/ES_GRAFONONORIENTATO/ES_GRAFONONORIENTATO.py
@@ -1,47 +1,93 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
 def main():
     v = int(input("Inserire il numero di nodi"))
-    grafo = creaMatrice(v)
-    stampaMatrice(grafo, v)
-    dict = adj2dict(grafo)
-    print(dict)
+    dict = creaDictDaNumNodi(v)
+    stampaDict(dict)
+    stampaMatrice(creaMatriceDaDict(dict, v), v)
 
 
-def creaMatrice(v):
-    grafo = []
+def creaMatriceDaNumNodi(v):
+    matrix = []
     for i in range(1, v + 1):
-        e = [int(i) for i in input(f"Inserire le vicinanze del nodo {i} (usare la ',' come separatore): ").split(',')]
+        e = [int(i) for i in input(f"Inserire le vicinanze del nodo {i} (usare la '.' come separatore): ").split('.')]
         colonna = [0 for dim in range(0, v)]
         for j in e:
             if j != i:
                 colonna[j - 1] = 1
-        grafo.append(colonna)
+        matrix.append(colonna)
 
-    return grafo
+    return matrix
 
 
-def adj2dict(grafo):
+def creaDictDaNumNodi(v):
+    dict = {}
+    for r in range(0, v):
+        chiave = r + 1
+        occ = [int(i) for i in
+               input(f"Inserire le vicinanze del nodo {chiave} (usare la '.' come separatore): ").split('.')]
+        dict[chiave] = occ
+
+    return dict
+
+
+def creaDictDaMatrice(grafo):
     dict = {}
     for r in range(0, len(grafo)):
         chiave = r + 1
         occ = []
         for c in range(0, len(grafo)):
             if grafo[r][c] == 1:
-                occ.append(c+1)
+                occ.append(c + 1)
         dict[chiave] = occ
 
     return dict
 
 
-def stampaMatrice(grafo, v):
+def creaMatriceDaDict(dict, v):
+    matrix = []
+    for key, val in dict.items():
+        colonna = [0 for dim in range(0, v)]
+        for link in val:
+            colonna[link - 1] = 1
+        matrix.append(colonna)
+
+    return matrix
+
+
+def stampaMatrice(matrix, v):
     for r in range(0, v):
         print(" ")
         for c in range(0, v):
-            print(grafo[r][c], end=' ')
+            print(matrix[r][c], end=' ')
+
+
+def stampaDict(dict):
+    print("\n{")
+    for key, val in dict.items():
+        print(f"\t{key}: {val},")
+
+    print("}")
+
+
+def drawGrafo(dict):
+    G = nx.petersen_graph()
+    listaNodi = []
+    listaToupleLink = []
+    for key, val in dict.items():
+        listaNodi.append(key)
+        for i in val:
+            toupleLink = [int(key), int(i)]
+            listaToupleLink.append(tuple(toupleLink))
+
+    G.add_nodes_from(listaNodi)
+    G.add_nodes_from(listaToupleLink)
+    plt.subplot(121)
+    nx.draw(G, with_labels=True, font_weight='bold')
+
 
 
 if __name__ == '__main__':
     main()
-
-"""
-dizionario delle adiacenze
-"""
